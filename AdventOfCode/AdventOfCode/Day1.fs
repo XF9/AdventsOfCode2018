3c8d81e160d49b009c07eb11c8c95a867b6bd760
Michael Möhler <m.moehler@xf9.de>
2018-12-01 18:39:51 +0100

    Day1

---
+++ AdventOfCode/AdventOfCode/Day1.fs
@@ -0,0 +1,22 @@
+﻿module Day1
+
+let input = System.IO.File.ReadAllLines "Day1Input.txt"
+            |> Array.toList
+            |> List.map (fun value -> value |> int)
+
+let solveDay1Part1() =
+    printf "Solving Day 1, Part 1\n"
+    printf "%i\n\n" (List.sum input)
+
+let rec step operationList frequencyList currentFrequency =
+    match List.isEmpty operationList with
+    | true -> step input frequencyList currentFrequency
+    | false -> let newFrequenzy = currentFrequency + operationList.Head
+               match Set.contains newFrequenzy frequencyList with
+               | true -> newFrequenzy
+               | false -> step operationList.Tail (frequencyList.Add(newFrequenzy)) newFrequenzy
+
+let solveDay1Part2() =
+    printf "Solving Day 1, Part 2\n"
+    let result = step [] Set.empty 0
+    printf "%i" result
--- AdventOfCode/AdventOfCode/Program.fs
+++ AdventOfCode/AdventOfCode/Program.fs
@@ -1,6 +1,8 @@
 ﻿open System
+open Day1
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
+let main args =
+    solveDay1Part2()
+    System.Console.ReadKey() |> ignore
     0
